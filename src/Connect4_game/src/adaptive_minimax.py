3db1a7f735a75f92ed93ab535ac635fc7c1525ed
Mohammed Alwanis <151536650+Pb22j@users.noreply.github.com>
2026-03-17 08:34:21 +0300

    Update adaptive_minimax.py

--- src/Connect4_game/src/adaptive_minimax.py
+++ src/Connect4_game/src/adaptive_minimax.py
@@ -1,3 +1,25 @@
+'''
+GameHub
+Copyright (C) 2026 to:
+
+-Pb22j 
+-majedco03
+-AlwaleedAlmutairi
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+'''
+
 """
 actully i tried 3 times to implement a RL but it still not working as i tried some
 algorithmes and approaches I found on the internet like 
